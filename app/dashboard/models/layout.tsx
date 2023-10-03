71f7f5f6f45ecdf0b228c255dde55873dd5ecf7d
jcsmithf22 <jcsmithf22@gmail.com>
2023-10-03 15:50:55 -0500

    using suspense tag instead of loading.tsx

--- app/dashboard/models/layout.tsx
+++ app/dashboard/models/layout.tsx
@@ -1,10 +1,20 @@
+import { Suspense } from "react";
+import LoadingTable from "@/components/LoadingTable";
 export default async function ServerLayout(props: {
   children: React.ReactNode;
   sidebar: React.ReactNode;
 }) {
   return (
     <>
-      {props.children}
+      <Suspense
+        fallback={
+          <LoadingTable title="Models">
+            A list of all models in the database.
+          </LoadingTable>
+        }
+      >
+        {props.children}
+      </Suspense>
       {props.sidebar}
     </>
   );
--- app/dashboard/types/layout.tsx
+++ app/dashboard/types/layout.tsx
@@ -1,10 +1,21 @@
+import { Suspense } from "react";
+import LoadingTable from "@/components/LoadingTable";
+
 export default async function ServerLayout(props: {
   children: React.ReactNode;
   sidebar: React.ReactNode;
 }) {
   return (
     <>
-      {props.children}
+      <Suspense
+        fallback={
+          <LoadingTable title="Types">
+            A list of all attachment types in the database.
+          </LoadingTable>
+        }
+      >
+        {props.children}
+      </Suspense>
       {props.sidebar}
     </>
   );
