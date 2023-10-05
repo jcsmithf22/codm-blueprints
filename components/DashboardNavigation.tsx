c1ef3c2985513f472e23b2d3e59152fe3c4720dd
jcsmithf22 <jcsmithf22@gmail.com>
2023-10-05 14:01:16 -0500

    added framer motion to dashboard nav

--- components/DashboardNavigation.tsx
+++ components/DashboardNavigation.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { usePathname } from "next/navigation";
 
 import { classNames } from "@/utils/functions";
@@ -11,6 +12,7 @@ import {
   SwatchIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 type Icon = React.ForwardRefExoticComponent<
   Omit<React.SVGProps<SVGSVGElement>, "ref"> & {
@@ -46,31 +48,54 @@ const navigation: Navigation[] = [
 
 export default function DashboardNavigation() {
   const pathname = usePathname();
+  const [hoveredNavItem, setHoveredNavItem] = React.useState<string | null>(
+    null
+  );
+  const id = React.useId();
   // const pathname = "/dashboard";
   return (
     <nav className="flex flex-1 flex-col">
-      <ul role="list" className="-mx-2 space-y-1">
-        {navigation.map((item) => {
-          const selected = pathname === item.href;
+      <ul
+        role="list"
+        className="-mx-2 space-y-1"
+        onMouseLeave={() => setHoveredNavItem(null)}
+      >
+        {navigation.map(({ name, icon: Icon, href }) => {
+          const selected = pathname === href;
           return (
-            <li key={item.name}>
+            <li
+              key={name}
+              className="relative"
+              style={{
+                zIndex: hoveredNavItem === name ? 1 : 2,
+              }}
+            >
+              {hoveredNavItem === name && (
+                <motion.div
+                  layoutId={id}
+                  className="bg-gray-100 absolute inset-0"
+                  initial={{
+                    borderRadius: 6,
+                  }}
+                />
+              )}
+
               <Link
-                href={item.href}
+                href={href}
+                onMouseEnter={() => setHoveredNavItem(name)}
                 className={classNames(
-                  selected
-                    ? "bg-gray-100 text-blue-600"
-                    : "text-gray-700 hover:text-blue-600 hover:bg-gray-100",
-                  "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                  selected ? "text-blue-600" : "text-gray-700",
+                  "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold relative"
                 )}
               >
-                <item.icon
+                <Icon
                   className={classNames(
-                    "h-6 w-6 shrink-0 group-hover:text-blue-600",
+                    "h-6 w-6 shrink-0",
                     selected ? "text-blue-600" : "text-gray-400"
                   )}
                   aria-hidden="true"
                 />
-                {item.name}
+                {name}
               </Link>
             </li>
           );
