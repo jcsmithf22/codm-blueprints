16ab90475931b840343267629f1f43ed3642393a
jcsmithf22 <jcsmithf22@gmail.com>
2023-10-06 20:44:53 -0500

    Fixed mismatched urls

--- app/dashboard/attachments/page.tsx
+++ app/dashboard/attachments/page.tsx
@@ -23,7 +23,7 @@ export default function AttachmentsPage() {
                 placeholder="Search all columns..."
               />
               <Link
-                href="/dashboard/types/create"
+                href="/dashboard/attachments/create"
                 className="flex-shrink-0 block rounded-md bg-blue-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
               >
                 Add attachment
--- app/dashboard/models/page.tsx
+++ app/dashboard/models/page.tsx
@@ -24,7 +24,7 @@ export default function ServerComponent() {
                 placeholder="Search all columns..."
               />
               <Link
-                href="/dashboard/types/create"
+                href="/dashboard/models/create"
                 className="flex-shrink-0 block rounded-md bg-blue-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
               >
                 Add model
