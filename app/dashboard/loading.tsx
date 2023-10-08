1cbad960c2734011fdf884bc610f8cdb832dbd59
jcsmithf22 <jcsmithf22@gmail.com>
2023-10-08 11:11:25 -0500

    added suspense with loading state

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+const data: { [key: string]: Array<string> } = {
+  types: ["Types", "attachment types", "type"],
+  models: ["Models", "models", "model"],
+  attachments: ["Attachments", "attachments", "attachment"],
+};
+
+const Loading = () => {
+  const pathname = usePathname();
+  const text = pathname.split("/").pop() || "";
+  const [title, plural, singular] = data[text];
+
+  return (
+    <div className="fixed top-0 left-0 md:left-72 right-0 z-10 p-4">
+      <div className="p-4 flex lg:items-center flex-col lg:flex-row gap-8 rounded-lg bg-white border border-gray-200 shadow">
+        <div className="sm:flex-grow flex-shrink-0">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">
+            {title}
+          </h1>
+          <p className="mt-2 text-sm text-gray-700">
+            A list of all {plural} in the database.
+          </p>
+        </div>
+        <div className="flex gap-x-2 justify-between w-full lg:justify-end">
+          <Link
+            href="/dashboard/types/create"
+            className={cn(
+              buttonVariants({ variant: "default" }),
+              "flex-shrink-0"
+            )}
+          >
+            Add {singular}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
